940e9fd9c26d9121147a90554a0e020e4bb265b2
John-David Dalton <john.david.dalton@gmail.com>
2014-12-31 21:48:44 -0600

    Bulk update.

---
+++ chain.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'chain': require('./chain/chain'),
+  'lodash': require('./chain/lodash'),
+  'reverse': require('./chain/reverse'),
+  'tap': require('./chain/tap'),
+  'thru': require('./chain/thru'),
+  'toJSON': require('./chain/toJSON'),
+  'value': require('./chain/value'),
+  'valueOf': require('./chain/valueOf'),
+  'wrapperChain': require('./chain/wrapperChain'),
+  'wrapperReverse': require('./chain/wrapperReverse'),
+  'wrapperToString': require('./chain/wrapperToString'),
+  'wrapperValue': require('./chain/wrapperValue')
+};
