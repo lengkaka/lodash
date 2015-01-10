482150385ca0ac4369f2ea437ff818b82057762d
John-David Dalton <john.david.dalton@gmail.com>
2015-01-10 12:55:03 -0800

    Bulk update.

---
+++ chain.js
@@ -0,0 +1,27 @@
+import chain from './chain/chain';
+import lodash from './chain/lodash';
+import reverse from './chain/reverse';
+import tap from './chain/tap';
+import thru from './chain/thru';
+import toJSON from './chain/toJSON';
+import value from './chain/value';
+import valueOf from './chain/valueOf';
+import wrapperChain from './chain/wrapperChain';
+import wrapperReverse from './chain/wrapperReverse';
+import wrapperToString from './chain/wrapperToString';
+import wrapperValue from './chain/wrapperValue';
+
+export default {
+  'chain': chain,
+  'lodash': lodash,
+  'reverse': reverse,
+  'tap': tap,
+  'thru': thru,
+  'toJSON': toJSON,
+  'value': value,
+  'valueOf': valueOf,
+  'wrapperChain': wrapperChain,
+  'wrapperReverse': wrapperReverse,
+  'wrapperToString': wrapperToString,
+  'wrapperValue': wrapperValue
+};
